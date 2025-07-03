f9e06fc0ec393167e103e10e71624cfbc2311639
Manoel Santiago <manoel.santiago1@gmail.com>
2025-07-03 05:16:42 -0500

    criação de pasta

---
+++ cria-pasta.py
@@ -0,0 +1,7 @@
+# criar uma aplicação que crie uma pasta com clique direito do mouse
+
+import pyautogui
+
+pyautogui.rightClick(x=1285,y=1013,duration=2)
+pyautogui.click(x=1285+92,y=1013-163,duration=2)
+pyautogui.click(x=1285+92-436,y=1013-163-567,duration=2)
